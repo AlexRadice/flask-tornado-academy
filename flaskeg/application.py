dec938fad12846e96d1b1cb345e2e69f5506be18
Alex Radice <alex.radice@bjss.com>
2017-07-04 10:31:48 +0000

    Some jinja template logic and a simple model

--- flaskeg/application.py
+++ flaskeg/application.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
 if __name__ == '__main__':
+    developers = [{
+        'name': 'Alex Radice',
+        'cell': 'A',
+        'favourite_fruit': 'grapefruit'
+        }]
     app = Flask('flaskeg')
 
     @app.route('/')
     def index():
         "Home page"
-        return render_template('index.html')
+        return render_template('index.html', developers=developers)
 
     app.run(port=11000, debug=True) 
