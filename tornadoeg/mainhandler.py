5bdac922f04185eec3c1f02d48107dc808783325
Alex Radice <alex.radice@bjss.com>
2017-07-04 13:50:56 +0000

    Use coroutine instead of callback

--- tornadoeg/mainhandler.py
+++ tornadoeg/mainhandler.py
@@ -2,20 +2,18 @@ from datetime import datetime
 import tornado.web
 import tornado.ioloop
 import tornado.httpclient
+import tornado.gen
 
 class MainHandler(tornado.web.RequestHandler):
     "Handler for the root page"
 
-    @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def get(self):
         "Respond to a GET request"
         print("START {} handling {}".format(self.__class__.__name__, self._request_summary()))
         http_client = tornado.httpclient.AsyncHTTPClient()
         print("START Backend fetch....")
-        http_client.fetch('http://www.google.com', callback=self.on_respond)
-
-    def on_respond(self, response):
-        "Handle response to fetch from backend store"
+        response = yield http_client.fetch('http://www.google.com')
         print("FINISH Backend fetch response status={}".format(response.code))
         if response.error:
             response_text = "Error {} from backend".format(response.error)
