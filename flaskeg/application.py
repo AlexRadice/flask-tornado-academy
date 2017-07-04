f9f1163a8a2705ec1e10f5652386a4341c2ba6eb
Alex Radice <alex.radice@bjss.com>
2017-07-04 11:37:17 +0000

    Jinja customisation

--- flaskeg/application.py
+++ flaskeg/application.py
@@ -8,9 +8,11 @@ from flask_principal import Principal
 
 app = Flask('flaskeg')
 app.secret_key = "really_secret_key"
+
+
+# Authentication
 login_manager = LoginManager()
 login_manager.init_app(app)
-principals = Principal(app)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -18,9 +20,14 @@ def load_user(user_id):
     user = get_user(user_id)
     return FlaskLoginUser(user)
 
+
+# Authorisation
+principals = Principal(app)
 principals.identity_loader(load_identity)
 principals.identity_saver(save_identity)
 
+
+# Controllers
 @app.route('/')
 def index():
     "Home page"
@@ -28,6 +35,26 @@ def index():
 
 app.add_url_rule('/developer', view_func=DeveloperDetailsController.as_view('developer'))
 app.add_url_rule('/login', view_func=LoginController.as_view('login'))
+
+
+
+# Jinja customisation
+def get_version():
+    "Get application version"
+    return '0.9.9.9.9.9.9.9'
+
+def polite(name):
+    "Return an extra polite name"
+    return u"Sir {}".format(name)
+
+def is_cellA(cell):
+    "Return True if this is cell A"
+    return cell == 'A'
+
+app.jinja_env.globals['get_version'] = get_version
+app.jinja_env.filters['polite'] = polite
+app.jinja_env.tests['is_cellA'] = is_cellA
+
 
 if __name__ == '__main__':
     app.run(port=11000, debug=True) 
