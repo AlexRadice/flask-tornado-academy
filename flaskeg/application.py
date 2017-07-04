51ccb26bf0af473e76d15c1338d4a526fd684519
Alex Radice <alex.radice@bjss.com>
2017-07-04 10:28:53 +0000

    Add a template with no logic

--- flaskeg/application.py
+++ flaskeg/application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 if __name__ == '__main__':
     app = Flask('flaskeg')
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     @app.route('/')
     def index():
         "Home page"
-        return '<html><body><h1>This is the home page!</h1></body></html>'
+        return render_template('index.html')
 
     app.run(port=11000, debug=True) 
