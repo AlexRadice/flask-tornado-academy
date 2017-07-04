2eb63d94692adaeff0f9691120a7d6772ea1e4b8
Alex Radice <alex.radice@bjss.com>
2017-07-04 13:52:29 +0000

    Call two fetches in series

--- tornadoeg/mainhandler.py
+++ tornadoeg/mainhandler.py
@@ -12,13 +12,20 @@ class MainHandler(tornado.web.RequestHandler):
         "Respond to a GET request"
         print("START {} handling {}".format(self.__class__.__name__, self._request_summary()))
         http_client = tornado.httpclient.AsyncHTTPClient()
-        print("START Backend fetch....")
-        response = yield http_client.fetch('http://www.google.com')
-        print("FINISH Backend fetch response status={}".format(response.code))
-        if response.error:
-            response_text = "Error {} from backend".format(response.error)
+
+        print("START Backend fetch 1....")
+        response1 = yield http_client.fetch('http://www.google.com')
+        print("FINISH Backend fetch 1 response status={}".format(response1.code))
+        print("START Backend fetch 2....")
+        response2 = yield http_client.fetch('http://search.yahoo.com')
+        print("FINISH Backend fetch 2 response status={}".format(response2.code))
+
+        if response1.error:
+            response_text = "Error {} from backend".format(response1.error)
+        elif response2.error:
+            response_text = "Error {} from backend".format(response2.error)
         else:
-            response_text = "{} bytes retrieved from backend".format(response.headers['Content-Length'])
+            response_text = "{} bytes retrieved from backend 1, {} bytes from backend 2".format(response1.headers['Content-Length'], response2.headers['Content-Length'])
 
         self.finish("<html><body><h1>This is the Home Page!</h1>" + 
             "<p>{}</p>".format(response_text) +
