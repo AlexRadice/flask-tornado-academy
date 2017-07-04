8e93907227dfeb304f7acbfab643255eb59fec79
Alex Radice <alex.radice@bjss.com>
2017-07-04 14:01:16 +0000

    Callback tunnelling

--- tornadoeg/datastore.py
+++ tornadoeg/datastore.py
@@ -4,14 +4,11 @@ import tornado.gen
 class DataStore(object):
     "Data store class"
 
-    @tornado.gen.engine
     def get_google(self, callback=None):
         "Get data from www.google.com"
         http_client = tornado.httpclient.AsyncHTTPClient()
         print("START Backend fetch from www.google.com  ....")
-        response  = yield http_client.fetch('http://www.google.com')
-        print("FINISH Backend fetch from www.google.com response status={}".format(response.code))
-        callback(response)
+        http_client.fetch('http://www.google.com', callback=callback)
 
     @tornado.gen.coroutine
     def get_yahoo(self):
